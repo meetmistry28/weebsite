18d86a0c5443d7f3c1a4467fc082fac070eb5239
ronu19 <ronakyt77@gmail.com>
2024-06-04 17:07:56 +0530

    v1.5

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
+// document.querySelector('#contact-form').addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     e.target.elements.name.value = '';
+//     e.target.elements.email.value = '';
+//     e.target.elements.message.value = '';
+//   });
+
+  $(document).ready(function (){
+    console.log("Yes Its Working");
+
+    $(".explore")
+    (function(){
+
+    })
+
+
   });
